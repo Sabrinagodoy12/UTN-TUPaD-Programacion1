d10368271c449cc9742e4eb3ca64f4cee027d294
Sabrina Godoy <sabrygodoy1@gmail.com>
2025-10-28 16:17:11 -0300

    tp recursividad ejercicio 1

---
+++ unidad-9/tp-recursividad.py
@@ -0,0 +1,25 @@
+# 1) Crea una función recursiva que calcule el factorial de un número. Luego, utiliza esa
+# función para calcular y mostrar en pantalla el factorial de todos los números enteros
+# entre 1 y el número que indique el usuario
+
+# import sys
+# sys.setrecursionlimit(20000)
+
+# def factorial(num):
+#     return 1 if num == 0 else num * factorial(num - 1)
+
+# def mostrar_factoriales(actual, limite):
+#     if actual > limite:
+#         return
+#     print(f"El factorial de: {actual} es: {factorial(actual)}")
+#     mostrar_factoriales(actual + 1, limite)
+
+# numero = int(input("Ingrese un número para calcular su factorial: "))
+
+# mostrar_factoriales(1, numero)
+
+
+
+# 2) Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición
+# indicada. Posteriormente, muestra la serie completa hasta la posición que el usuario
+# especifique.
